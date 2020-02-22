0e84291ed5d8b0cfae6fa0e1e54ff69888f8ff6c
Wad Werton <wad_werton@mail.ru>
2020-02-22 13:26:55 +0300

    feat: add solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    if (str[0] === '-') return str.substring(1, str[str.length]).split('').reverse().join('');
+    return str.split('').reverse().join('');
 }
